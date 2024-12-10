11a82a2bd07130c8a1285016940b3b37daab7dc0
Jeffreyfrmn <jeffreywaynefreemanjr@gmail.com>
2024-12-10 01:04:39 -0700

    Added additional identifiers.

--- data/train/counter.py
+++ data/train/counter.py
@@ -9,16 +9,35 @@ dictionary = dict()
 for file in os.listdir(f"{os.getcwd()}\\data\\train"):
     if file.endswith(".csv"):
         print(file)
-        csv = open(f"{path}\\{file}", "r")
+        csv = open(f"{path}\\{file}", "r", errors='ignore')
         row_number = 0
         for row in csv:
             entry = (row.strip().split(',', 3))
             text = entry[3]
             id = entry[0]
             for word in text.split():
-                word = word.strip('"').strip('?').strip('.').strip(',')
+                if len(word) > 1 and word[-1] in string.punctuation:
+                    word = word[:-1]
+                if len(word) > 1 and word[0] in string.punctuation:
+                    word = word[1:]
+                word = word.strip('"').strip('?').strip('.').strip(',').strip('!').strip(';')
+                word = word.lstrip('(').rstrip(')')
                 word = word.lower()  # filter with capitalization disabled
                 if word.isnumeric():
+                    continue
+                if len(word) == 1:
+                    continue
+                if '@' in word:
+                    continue
+                if '/' in word:
+                    continue
+                if word == "":
+                    continue
+                if word[-1] == ':':
+                    continue
+                if word[:-1].isnumeric() and word[-1] == 's':
+                    continue
+                if word.replace('-', '').isnumeric():
                     continue
                 if (word in dictionary):
                     dictionary[word]["count"] += 1
@@ -44,7 +63,7 @@ for word, data in max_count.items():
     if word in max_occur:
         max_words[word] = data["count"]
 
-sorted_items = sorted(max_words.items(), key=lambda kv: (kv[1], kv[0]))
-
-for word, count in sorted_items[::-1]:
-    print(word)
+f = open(f"{path}\\all_words.txt", "w")
+for word in max_words.keys():
+    f.write(word + '\n')
+f.close()
